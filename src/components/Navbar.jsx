a808df19747911cc66e19fa051d77be644139968
Jigarkumar Raval <jigarkumar1230@gmail.com>
2025-02-17 14:52:13 +0530

    change in css

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -29,9 +29,9 @@ const NavBar = () => {
   ];
 
   return (
-    <div className="flex justify-between items-center w-full h-20 px-4 text-white bg-black fixed">
+    <div className="flex justify-between items-center w-full h-20 px-4 text-white bg-black fixed ">
       <div>
-        <h1 className="text-5xl font-signature ml-2">Jigar</h1>
+        <h1 className="text-3xl name ml-2">Jigar</h1>
       </div>
 
       <ul className="hidden md:flex">
